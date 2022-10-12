b08253e72b3824f0b6f55128af8e14595909990c
NurulAuliaDewi <1197050102@student.uinsgd.ac.id>
2022-10-12 21:49:56 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ const cors = require('cors');
 const app = express();
 const {db} = require('./model/dbConnection'); 
 const moment = require('moment');
+const multer = require('multer');
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './src/assets')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "--" + file.originalname);
+    },
+});
+const upload = multer({ storage: fileStorageEngine });
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/assets', express.static('assets'));
 
 //GET db sewa (WORK)
 app.get("/api/readData", (req, res) =>{
@@ -94,13 +103,15 @@ app.put("/api/updateStatus", (req, res) =>{
     });
 });
 
-//POST Bukti Pembayaran (ERROR) *lom kelar
-app.post("/api/createBuktiPembayaran", (req, res) =>{
-    const bukti_pembayaran = req.file.path;
-    const bukti_pembayaran_denda = req.file.path;
-    
-    const sqlQuery = "INSERT INTO sewa (bukti_pembayaran, bukti_pembayaran_denda) VALUE (?, ?)";
-    db.query(sqlQuery, [bukti_pembayaran, bukti_pembayaran_denda], (err, result) =>{
+//PUT update bukti pembayaran (WORK)
+app.put("/buktipembayaran", upload.fields([{name: "pembayaran"}, {name: "denda"}]), (req, res) =>{
+    const bukti_pembayaran =  req.files.pembayaran[0].filename;
+    const bukti_pembayaran_denda =  req.files.denda[0].filename;
+    const id_sewa = req.body.id_sewa;
+    console.log(bukti_pembayaran,bukti_pembayaran_denda);
+
+    const sqlQuery = "UPDATE sewa SET bukti_pembayaran = ?, bukti_pembayaran_denda = ? WHERE id_sewa = ?";
+    db.query(sqlQuery, [bukti_pembayaran, bukti_pembayaran_denda, id_sewa], (err, result) =>{
         if (err) {
             return res.status(500).json({
                 message: "Ada kesalahan",
@@ -108,12 +119,17 @@ app.post("/api/createBuktiPembayaran", (req, res) =>{
             });
         }
         else{
-            res.send(result);
+            res.json({
+                status: "sukses",
+                id: id_sewa,
+                bayar: bukti_pembayaran,
+                denda : bukti_pembayaran_denda
+            });
+        
             console.log(result);
         }
     });
 });
-
 
 app.listen(5000, () => {
     console.log('Server running in port 5000..');
